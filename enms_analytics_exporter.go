5ca5e7c8a289d03bef38fbec3b2e5ee1f0d18834
Mohamed El Kholy <melkholy@eventumsolutions.com>
2023-01-31 10:25:31 +0200

    fix files

---
+++ enms_analytics_exporter.go
@@ -0,0 +1,141 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package enmsAnalyticsExporter // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/fileexporter"
+
+import (
+	"context"
+	"encoding/binary"
+	"io"
+	"sync"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/pdata/plog"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+)
+
+// Marshaler configuration used for marhsaling Protobuf
+var tracesMarshalers = map[string]ptrace.Marshaler{
+	formatTypeJSON:  &ptrace.JSONMarshaler{},
+	formatTypeProto: &ptrace.ProtoMarshaler{},
+}
+var metricsMarshalers = map[string]pmetric.Marshaler{
+	formatTypeJSON:  &pmetric.JSONMarshaler{},
+	formatTypeProto: &pmetric.ProtoMarshaler{},
+}
+var logsMarshalers = map[string]plog.Marshaler{
+	formatTypeJSON:  &plog.JSONMarshaler{},
+	formatTypeProto: &plog.ProtoMarshaler{},
+}
+
+// exportFunc defines how to export encoded telemetry data.
+type exportFunc func(e *enmsAnalyticsExporter, buf []byte) error
+
+// enmsAnalyticsExporter is the implementation of enmsAnalyticsExporter that sends telemetry data to enms analytics layer
+
+type enmsAnalyticsExporter struct {
+	path  string
+	file  io.WriteCloser
+	mutex sync.Mutex
+
+	tracesMarshaler  ptrace.Marshaler
+	metricsMarshaler pmetric.Marshaler
+	logsMarshaler    plog.Marshaler
+
+	compression string
+	compressor  compressFunc
+
+	formatType string
+	exporter   exportFunc
+}
+
+func (e *enmsAnalyticsExporter) Capabilities() consumer.Capabilities {
+	return consumer.Capabilities{MutatesData: false}
+}
+
+func (e *enmsAnalyticsExporter) ConsumeTraces(_ context.Context, td ptrace.Traces) error {
+	buf, err := e.tracesMarshaler.MarshalTraces(td)
+	if err != nil {
+		return err
+	}
+	buf = e.compressor(buf)
+	return e.exporter(e, buf)
+}
+
+func (e *enmsAnalyticsExporter) ConsumeMetrics(_ context.Context, md pmetric.Metrics) error {
+	buf, err := e.metricsMarshaler.MarshalMetrics(md)
+	if err != nil {
+		return err
+	}
+	buf = e.compressor(buf)
+	return e.exporter(e, buf)
+}
+
+func (e *enmsAnalyticsExporter) ConsumeLogs(_ context.Context, ld plog.Logs) error {
+	buf, err := e.logsMarshaler.MarshalLogs(ld)
+	if err != nil {
+		return err
+	}
+	buf = e.compressor(buf)
+	return e.exporter(e, buf)
+}
+
+func exportMessageAsLine(e *enmsAnalyticsExporter, buf []byte) error {
+	// Ensure only one write operation happens at a time.
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	if _, err := e.file.Write(buf); err != nil {
+		return err
+	}
+	if _, err := io.WriteString(e.file, "\n"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func exportMessageAsBuffer(e *enmsAnalyticsExporter, buf []byte) error {
+	// Ensure only one write operation happens at a time.
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+	// write the size of each message before writing the message itself.  https://developers.google.com/protocol-buffers/docs/techniques
+	// each encoded object is preceded by 4 bytes (an unsigned 32 bit integer)
+	data := make([]byte, 4, 4+len(buf))
+	binary.BigEndian.PutUint32(data, uint32(len(buf)))
+	data = append(data, buf...)
+	if err := binary.Write(e.file, binary.BigEndian, data); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *enmsAnalyticsExporter) Start(context.Context, component.Host) error {
+	return nil
+}
+
+// Shutdown stops the exporter and is invoked during shutdown.
+func (e *enmsAnalyticsExporter) Shutdown(context.Context) error {
+	return e.file.Close()
+}
+
+func buildExportFunc(cfg *Config) func(e *enmsAnalyticsExporter, buf []byte) error {
+	if cfg.FormatType == formatTypeProto {
+		return exportMessageAsBuffer
+	}
+	// if the data format is JSON and needs to be compressed, telemetry data can't be written to file in JSON format.
+	if cfg.FormatType == formatTypeJSON && cfg.Compression != "" {
+		return exportMessageAsBuffer
+	}
+	return exportMessageAsLine
+}
--- go.mod
+++ go.mod
@@ -1,13 +1,10 @@
 module github.com/mohamedtaher567/enmsAnalyticsExporter
 
-
 go 1.18
 
 require (
 	github.com/klauspost/compress v1.15.13
-	github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal v0.68.0
 	github.com/open-telemetry/opentelemetry-collector-contrib/internal/sharedcomponent v0.68.0
-	github.com/stretchr/testify v1.8.1
 	go.opentelemetry.io/collector v0.68.0
 	go.opentelemetry.io/collector/component v0.68.0
 	go.opentelemetry.io/collector/confmap v0.68.0
@@ -18,7 +15,6 @@ require (
 
 require (
 	github.com/cenkalti/backoff/v4 v4.2.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -28,8 +24,6 @@ require (
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/collector/featuregate v0.68.0 // indirect
 	go.opentelemetry.io/otel v1.11.2 // indirect
@@ -44,5 +38,4 @@ require (
 	google.golang.org/genproto v0.0.0-20220822174746-9e6da59bd2fc // indirect
 	google.golang.org/grpc v1.51.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
